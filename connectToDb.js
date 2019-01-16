1209f98317d9ce38822f2efcfaf8303c1c29fedd
Jorge <jorge@Nathans-MacBook-Pro-3.local>
2019-01-15 20:09:49 -0800

    exports connection

--- connectToDb.js
+++ connectToDb.js
@@ -6,29 +6,18 @@ const Firestore = require('@google-cloud/firestore')
 */
 
 const _projectId = //your prj id
-const keyFile = "location of file"
+const keyFile = //"location of file"
 
 
 /* create a new instace of your db */
 const firestore = new Firestore({
 	projectId: _projectId,
 	keyFilename: keyFile,
+	timestampsInSnapshots: true,
 });
 
-/* call the document from your firestore collection 
-	 ex: docName = 'Collectionname/documentName'*/
-const docName = //your documents name
-const document = firestore.doc(docName);
+/* Export the firestore const to use in different files if wanted 
+ * Global variables like this is generally a  bad programming method
+*/
 
-/* Enter data into the document */
-document.set({
-	CollectionName: 'Hello',
-}).then(() => {
-	console.log('item has been appended')
-});
-
-	
-
-
-
-
+exports.firestore = firestore;
